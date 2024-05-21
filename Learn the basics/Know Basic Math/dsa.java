a81426066efef5a97d7f896a8efd08afec600240
aryanmundra <developer.aryann@gmail.com>
2024-05-21 23:19:25 +0530

    Strings easy sums

---
+++ Learn the basics/Know Basic Math/dsa.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+
+class dsa {
+    public static void main(String args[]) {
+        int arr = { 2, 3, 6, 4, 1 };
+        Arrays.sort(arr);
+        System.out.print(arr);
+    }
+}
